ff50d9d599db9b3912e939e6bf01022d52fffdfb
Linxi Wu <lwu9@dt-login03.delta.ncsa.illinois.edu>
2026-05-22 08:37:27 -0500

    e03a

--- src/qwenvl/model/modeling_qwen3_vl.py
+++ src/qwenvl/model/modeling_qwen3_vl.py
@@ -881,7 +881,7 @@ class Qwen3VLTextModel(Qwen3VLPreTrainedModel):
 
         attention_mask = create_causal_mask(
             config=self.config,
-            inputs_embeds=inputs_embeds,
+            input_embeds=inputs_embeds,
             attention_mask=attention_mask,
             cache_position=cache_position,
             past_key_values=past_key_values,
